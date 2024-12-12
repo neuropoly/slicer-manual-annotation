940ea4cec77c98bcc135a16486d6c5a86b6bc619
maximeb <mbmsc2025@gmail.com>
2024-12-12 11:15:46 -0500

    backup before cleaning

--- SlicerCART/src/utils/debugging_helpers.py
+++ SlicerCART/src/utils/debugging_helpers.py
@@ -1,4 +1,5 @@
 import functools
+import inspect
 from utils.global_variables import ENABLE_DEBUG
 
 
@@ -104,15 +105,58 @@ def enter_function(func):
         # else:
         #     result = func(self)
 
-        if (len(args) == 1 and args[0] is False):
-            #ToDo: Find how to distinguish False as an argument from False
-            # from default args Tuple. This is currently a LIMITATION only
-            # if the first argument is False
+        # Use inspect to get argument names
+        sig = inspect.signature(func)
+        print('sig', sig)
+        print('sig paragm', list(sig.parameters))
+        args_list = list(sig.parameters)
+        print('len args list', len(args_list))
+
+        if len(args_list) == 1:
+            print('args list has opnly self')
             result = func(self)
         else:
-            print('arg not none')
+            print('***** has not only self')
             result = func(self, *args, **kwargs)
-            print('function worked')
+
+        # # Handle the case where the first and only argument is explicitly False
+        # if len(args) == 1 and args[0] is False:
+        #     print('mean that the function has no argument or 1 argument that '
+        #           'is false')
+        #     print("Single argument is explicitly False")
+        #     print('self', self)
+        #     print('args 0 type', type(args[0]))
+        #     result = func(self)  # Pass the argument explicitly, including False
+        # else:
+        #     # Call the original function with the provided arguments
+        #     print('else dd')
+        #     result = func(self, *args, **kwargs)
+
+
+        # if (len(args) == 1 and args[0] is False):
+        #     print('self, ', self)
+        #     print('testing', args[0] == self)
+        #     if ((args[0] == self) == False):
+        #         print('args 0 = self == false')
+        #         result = func(self)
+        #         # result = func(self, args[0])
+        #     else:
+        #         #ToDo: Find how to distinguish False as an argument from False
+        #         # from default args Tuple. This is currently a LIMITATION only
+        #         # if the first argument is False
+        #         print('len 1')
+        #         result = func(self)
+        #         # result = func(self)
+        #         print('len 1 worked')
+        # else:
+        #     if len(args) == 2 and args[1] == False:
+        #         print('case scenario set to false object')
+        #         result = func(self, args[1])
+        #         print('terrer')
+        #     else:
+        #         print('arg not none')
+        #         result = func(self, *args, **kwargs)
+        #         print('function worked')
 
         return result
 
